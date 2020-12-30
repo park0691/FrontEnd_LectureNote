5c2b8f0dd16fa7586a982bae0f2b3007f3e905c8
Dae-Eon, Park <mem29238@gmail.com>
2020-12-31 00:20:47 +0900

    useInput Hook 상태 관리 useReducer로 변경
    * 인풋 초기화시 객체 메소드 keys 함수와 배열 내장 함수 reduce 사용

--- React/begin-react/src/useInputs.js
+++ React/begin-react/src/useInputs.js
@@ -1,12 +1,33 @@
-import { useState, useCallback } from 'react';
+import { useState, useReducer, useCallback } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      return {
+        ...state,
+        [action.name]: action.value
+      };
+    case 'INPUT_RESET':
+      return Object.keys(state).reduce((acc, current) => {
+        acc[current] = '';
+        return acc;
+      }, {});
+    default:
+      return state;
+  }
+}
 
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
   const onChange = useCallback(e => {
     const { name, value } = e.target;
-    setForm(form => ({ ...form, [name]: value}));
+    dispatch({
+      type: 'INPUT_CHANGE',
+      name,
+      value
+    });
   }, []);
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  const reset = useCallback(() => dispatch({ type: 'INPUT_RESET' }), []);
 
   return [form, onChange, reset];
 }
