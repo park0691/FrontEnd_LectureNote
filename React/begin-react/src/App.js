15625908ca272a45753d2c1739a0717fd6d590d4
Dae-Eon, Park <mem29238@gmail.com>
2020-12-30 22:45:06 +0900

    useState 사용한 로직을 useReducer로 변경하기

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useMemo, useCallback } from 'react';
+import { useRef, useReducer, useMemo, useCallback } from 'react';
 import Counter from './Counter';
 import CreateUser from './CreateUser';
 import Hello from './Hello';
@@ -11,20 +11,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: '',
-  });
-  const { username, email } = inputs;
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  }, [inputs]);
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -43,37 +35,92 @@ function App() {
       email: 'lee@pusan.ac.kr',
       active: false
     }
-  ]);
-  /* useRef -> 컴포넌트 리렌더링되도 값 유지. 값이 바껴도 컴포넌트 리렌더링되지 않는다. */
+  ]
+};
+
+/*
+  useState vs. useReducer
+  컴포넌트에서 관리하는 값이 하나고, 단순한 값이면 useState로 관리하는 것이 편리함.
+  컴포넌트에서 관리하는 값이 여러 개가 되어 구조가 복잡하거나, users 배열에 추가하거나 삭제할 때 useReducer가 편리할 수 있음.
+  개발자의 기호에 따라 알맞게 사용하도록 한다.
+*/
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        // input 초기화 (기존 useState - input 초기화와 user 배열 업데이트 따로했는데 동시에 가능)
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id
+            ? { ...user, active: !user.active }
+            : user
+          )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  /* useCallback -> 함수 재사용 */
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    }
-    // setUsers([...users, user]);
-    setUsers(users => users.concat(user));
-    setInputs({
-      username: '',
-      email: '',
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
   }, [username, email]);
 
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id !== id));
-  }, []);
-
   const onToggle = useCallback(id => {
-    setUsers(users => users.map(
-      user => user.id === id
-        ? { ...user, active: !user.active }
-        : user
-    ));
-  }, []);
-  /* useMemo -> 연산된 값 재사용 */
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  });
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  });
+
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
@@ -86,8 +133,8 @@ function App() {
       />
       <UserList
         users={users}
-        onRemove={onRemove}
         onToggle={onToggle}
+        onRemove={onRemove}
       />
       <div>활성 사용자 수: {count}</div>
     </>
--- React/begin-react/src/Counter.js
+++ React/begin-react/src/Counter.js
@@ -1,13 +1,32 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+/*
+  reducer의 사용.
+    상태 업데이트 로직이 컴포넌트 밖에 있다.
+    dispatch : action 발생시킨다.
+*/
 function Counter() {
-  const [number, setNumber] = useState(0);
-
+  // const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
   const onIncrease = () => {
-    setNumber(prevNumber => prevNumber + 1);
+    dispatch({
+      type: 'INCREMENT'
+    });
   };
   const onDecrease = () => {
-    setNumber(prevNumber => prevNumber - 1);
+    dispatch({
+      type: 'DECREMENT'
+    });
   };
 
   return (
