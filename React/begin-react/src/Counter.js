50306144dd4365ea670a780a0cc67736ad414217
Dae-Eon, Park <mem29238@gmail.com>
2020-12-31 16:35:49 +0900

    Counter, Hello 컴포넌트 클래스형 컴포넌트로 변경

--- React/begin-react/src/Counter.js
+++ React/begin-react/src/Counter.js
@@ -1,41 +1,111 @@
-import { useState, useReducer } from 'react';
+import { Component } from 'react';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      throw new Error('Unhandled action');
+class Counter extends Component {
+
+  constructor(props) {
+    super(props);
+    // this.handleIncrease = this.handleIncrease.bind(this);
+    // this.handleDecrease = this.handleDecrease.bind(this);
+    this.state = {
+      counter: 0,
+      fixed: 1,
+      updateMe: {
+        toggleMe: false,
+        dontChangeMe: 1
+      }
+    };
+  }
+  // 불변성 유지 필요 X
+  /*
+    +3될 것 같지만 +1 된다.
+    state는 비동기로 업데이트되므로.
+  */
+  handleIncrease = () => {
+    this.setState({    
+      counter: this.state.counter + 1
+    });
+    this.setState({    
+      counter: this.state.counter + 1
+    });
+    this.setState({    
+      counter: this.state.counter + 1
+    });
+  }
+
+  handleDecrease = () => {
+    // this.setState({
+    //   counter: this.state.counter - 1
+    // });
+    /* -3 */
+    this.setState(state => ({
+      counter: state.counter - 1
+    }));
+    this.setState(state => ({
+      counter: state.counter - 1
+    }));
+    this.setState(state => ({
+      counter: state.counter - 1
+    }));
+  }
+
+  handleToggle = () => {
+    this.setState({
+      updateMe: {
+        // 객체에 대해서는 불변성 유지 필요
+        ...this.state.updateMe,
+        toggleMe: !this.state.updateMe.toggleMe
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+        <p>고정된 값 : {this.state.fixed}</p>
+      </div>
+    )
   }
 }
-/*
-  reducer의 사용.
-    상태 업데이트 로직이 컴포넌트 밖에 있다.
-    dispatch : action 발생시킨다.
-*/
-function Counter() {
-  // const [number, setNumber] = useState(0);
-  const [number, dispatch] = useReducer(reducer, 0);
-  const onIncrease = () => {
-    dispatch({
-      type: 'INCREMENT'
-    });
-  };
-  const onDecrease = () => {
-    dispatch({
-      type: 'DECREMENT'
-    });
-  };
 
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
-}
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//       throw new Error('Unhandled action');
+//   }
+// }
+// /*
+//   reducer의 사용.
+//     상태 업데이트 로직이 컴포넌트 밖에 있다.
+//     dispatch : action 발생시킨다.
+// */
+// function Counter() {
+//   // const [number, setNumber] = useState(0);
+//   const [number, dispatch] = useReducer(reducer, 0);
+//   const onIncrease = () => {
+//     dispatch({
+//       type: 'INCREMENT'
+//     });
+//   };
+//   const onDecrease = () => {
+//     dispatch({
+//       type: 'DECREMENT'
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <h1>{number}</h1>
+//       <button onClick={onIncrease}>+1</button>
+//       <button onClick={onDecrease}>-1</button>
+//     </div>
+//   );
+// }
 
 export default Counter;
--- React/begin-react/src/Hello.js
+++ React/begin-react/src/Hello.js
@@ -1,15 +1,32 @@
-function Hello({ color, name, isSpecial }) {
-  return (
-    <div style={{
-      color
-    }}>
-      {isSpecial ? <b>*</b> : null}
-      안녕하세요 {name}
-    </div>
-  );
+import { Component } from 'react';
+
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음'
+  };
+  render() {
+    const { color, isSpecial, name } = this.props;
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    )
+  }
 }
 
-Hello.defaultProps = {
-  name: '이름없음'
-};
+// function Hello({ color, name, isSpecial }) {
+//   return (
+//     <div style={{
+//       color
+//     }}>
+//       {isSpecial ? <b>*</b> : null}
+//       안녕하세요 {name}
+//     </div>
+//   );
+// }
+
+// Hello.defaultProps = {
+//   name: '이름없음'
+// };
 export default Hello;
