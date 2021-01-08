00483f7e0c6c9c12066db9e0142ddabbc0097d0f
Dae-Eon, Park <mem29238@gmail.com>
2021-01-08 16:12:10 +0900

    API 연동 - reducer로 변경

--- React/api-integrate/src/Users.js
+++ React/api-integrate/src/Users.js
@@ -1,30 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null
+  });
   const fetchUsers = async () => {
+    dispatch({ type: 'LOADING' });
     try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users/'
       );
-      setUsers(response.data);
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      console.log(e.response);
-      setError(e);
+      dispatch({ type: 'ERROR', error: e});
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  const {loading, data: users, error} = state;
   if (loading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생하였습니다.</div>;
   if (!users) return null;
